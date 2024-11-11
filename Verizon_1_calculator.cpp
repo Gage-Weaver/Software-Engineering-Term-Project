df9bad8f93bd05600adb6582a1cfe4d476ab985a
AryanGhorpade21 <143010639+AryanGhorpade21@users.noreply.github.com>
2024-11-11 15:38:21 -0600

    Verizon_1.1_Calculator

--- Verizon_1_calculator.cpp
+++ Verizon_1_calculator.cpp
@@ -2,66 +2,73 @@
 #include <cmath>
 using namespace std;
  
-int addition(int a, int b) {
-    return a + b;
-}
- 
-int subtraction(int a, int b) {
-    return a - b;
-}
- 
-double division(int a, int b) { // Change return type to double for division
-    if (b == 0) {
-        cout << "Error: Division by zero is not allowed." << endl;
-        return 0; // Return 0 in case of division by zero
-    }
-    return static_cast<double>(a) / b; // Cast to double for accurate division
-}
- 
-int multiplcation(int a, int b) {
-    return a * b;
-}
- 
-double power_raise(int a, int b) {
-    return pow(a,b);
-}
- 
-int main() {
-    int num_1;
-    int num_2;
-    char choice;
-     
-    //  Perform the operation based infinitely
-    while (true){
-        cout << "Enter a number: ";
-        cin >> num_1;
-        cout << endl;
-        cout << "Enter another number: ";
-        cin >> num_2;
-        cout << endl;
-        cout << "Enter operation to be performed (+, -, *, /, ^): ";
-        cin >> choice; // Read the choice from the user
 
+
+ class Calculator{
+    public:
+        char history[10][10] = {}; 
+ 
+    float addition(float a, float b) {
+        return a + b;
+    }
+    
+    float subtraction(float a, float b) {
+        return a - b;
+    }
+    
+    float division(float a, float b) { 
+        if (b == 0) {
+            cout << "Error: Division by zero is not allowed." << endl;
+            return 0; 
+        }
+        return a/b; 
+    }
+    
+    float multiplcation(float a, float b) {
+        return a * b;
+    }
+    
+    double power_raise(float a, float b) {
+        return pow(a,b);
+    }
+ }
+
+void operation(){
+    
+    float num_1;
+    float num_2;
+    char choice;
+    Calculator operater; 
+    while (true){
+    cout << "Enter a number: ";
+    cin >> num_1;
+    cout << endl;
+    cout << "Enter another number: ";
+    cin >> num_2;
+    cout << endl;
+    cout << "Enter operation to be performed (+, -, *, /, ^): ";
+    cin >> choice; // Read the choice from the user
+    
         switch (choice) {
             //  Addition
             case '+':
-                cout << num_1 << " + " << num_2 << " = " << addition(num_1, num_2) << endl;
+                cout << num_1 << " + " << num_2 << " = " << operater.addition(num_1,num_2) << endl;
                 break;
             //   Subtraction
             case '-':
-                cout << num_1 << " - " << num_2 << " = " << subtraction(num_1, num_2) << endl;
+                cout << num_1 << " - " << num_2 << " = " << operater.subtraction(num_1, num_2) << endl;
                 break;
             //    Multiplication
             case '*':
-                cout << num_1 << " * " << num_2 << " = " << multiplcation(num_1, num_2) << endl;
+                cout << num_1 << " * " << num_2 << " = " << operater.multiplcation(num_1, num_2) << endl;
                 break;
             //     Division
             case '/':
-                cout << num_1 << " / " << num_2 << " = " << division(num_1, num_2) << endl;
+                cout << num_1 << " / " << num_2 << " = " << operater.division(num_1, num_2) << endl;
                 break;
             //       Power
             case '^':
-                cout << num_1 << " ^ " << num_2 << " = " << power_raise(num_1, num_2) << endl;
+                cout << num_1 << " ^ " << num_2 << " = " << operater.power_raise(num_1, num_2) << endl;
                 break;
             //       Invalid input handling 
             default:
@@ -71,4 +78,9 @@ int main() {
         }
     }
     return 0;
+}
+
+int main() {
+    operation();
+    return 0 ; 
 }
