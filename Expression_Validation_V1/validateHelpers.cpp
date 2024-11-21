427f5d4e36df99c0f6bff071fb003bae23ee92ae
Gage Weaver <Gage_Weaver@ku.edu>
2024-11-21 12:22:31 -0600

    Create validateHelpers.cpp

---
+++ Expression_Validation_V1/validateHelpers.cpp
@@ -0,0 +1,16 @@
+#include "validatehelpers.h"
+
+// Check if the token is a number
+bool isNumber(const std::string& token) {
+    for (char c : token) {
+        if (!(c >= '0' && c <= '9' || c == '.')) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Check if the token is an operator
+bool isOperator(const std::string& token) {
+    return token == "+" || token == "-" || token == "*" || token == "/" || token == "%" || token == "**";
+}
