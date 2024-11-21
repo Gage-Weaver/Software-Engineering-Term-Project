0f9931b8ccfc7c704bb312a138cd096990b7917e
Gage Weaver <Gage_Weaver@ku.edu>
2024-11-21 12:21:53 -0600

    Create validateHelpers.h

---
+++ Expression_Validation_V1/validateHelpers.h
@@ -0,0 +1,10 @@
+#ifndef VALIDATEHELPERS_H
+#define VALIDATEHELPERS_H
+
+#include <string>
+
+// Function prototypes
+bool isNumber(const std::string& token);
+bool isOperator(const std::string& token);
+
+#endif
