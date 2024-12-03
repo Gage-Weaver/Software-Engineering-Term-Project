7013d113e52c95690a4449f381bafd9bddc7d2d7
Abel <146387252+abelherzberg@users.noreply.github.com>
2024-12-03 14:55:16 -0600

    Update Exceptions.cpp
    Reworked the Exceptions.cpp file to account for other errors in addition to divide by 0.

--- Exceptions.cpp
+++ Exceptions.cpp
@@ -2,27 +2,28 @@
 
 using namespace std;
 
-void division(){ //function for division operation 
-    float numerator, denominator, answer; //Defines the numerator, denominator, and answer as floats(decimal numbers) in order to handle non-whole number situations
-
-    std::cout << "Numerator: "; //Prompts the user to enter an input for the numerator
-    std::cin >> numerator; //Stores the value entered by the user as the float 'numerator' defined at the top of the function
-
-    std::cout << "Denominator: "; //Prompts the user to enter an input for the denominator
-    std::cin >> denominator; //Stores the value entered by the user as the float 'denominator' defined at the top of the function
-
-
-    while (denominator == 0){ //while loop that runs as long as the denominator value entered is zero
-        std::cout << "ERROR: The denominator value can't be zero for division operator. Enter a new denominator value: "; //print statement that explains the error of
-        //divide by 0 and prompts the user to enter a new denominator value, giving the user a chance to fix the error without having to re-run the program
-        std::cin >> denominator; //stores the new value entered by the user, which prompts the while loop to run again, confirming the new value they entered is valid before continuing 
-    }
-
-    answer = (numerator/denominator); //Defines the float 'answer' from the top of the function as the floating-point division of the numerator and denominator
-
-    std::cout << "Answer: " << answer << std::endl; //Outputs the answer to the user 
+//incorrect number of parenthesis case (too many open or closed parenthesis)
+void invalidParenthesis(){
+    //Prints the following explanation to the user, explaining why their expression with parenthesis included did not compute an answer
+    cout << "Error: Imbalance of parenthesis. There are not an equal number of open and closing parenthesis. Check to ensure each opening parenthesis has a corresponding closing parenthesis." << endl;
 }
 
-int main(){ //main function
-    division(); //single call to the division function. In the future this can be changed to a menu with a while loop in order to eliminate needing to rerun the program
+//divide by 0 case
+void divideByZero(){
+     //Prints the following explanation to the user, explaining why their division didn't compute a solution
+    cout << "Error: Divide by Zero. The divisor can't be zero." << endl;
 }
+
+//modulo by 0 case
+void moduloByZero(){
+    //prints the following explanation to the user, telling them why their modulo wasn't calculated
+    cout << "Error: Modulo by Zero. The modulo operation can't be done with 0 as the divisor." << endl; 
+}
+
+/*
+int main(){ //main function that runs each of the functions above as a test to prove they are outputting properly. It is commented out, as it's not necessary for full integration
+    invalidParenthesis();
+    divideByZero();
+    moduloByZero();
+}
+*/
