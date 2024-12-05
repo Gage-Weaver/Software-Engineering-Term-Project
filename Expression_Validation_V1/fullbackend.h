aade8d782c720a300213652ea40c9965d4b48cea
shayton23 <130596599+shayton23@users.noreply.github.com>
2024-12-05 11:40:56 -0600

    Commit of fullbackend.h 12/5
    This probably needs a little bit of an update in the top two lines. Other than that, this should be good.

---
+++ Expression_Validation_V1/fullbackend.h
@@ -0,0 +1,10 @@
+#ifndef allIncludes_H
+#define allIncludes_H
+#include <stdexcept>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <cctype> //For isdigit
+using namespace std;
