562bd0bc030b196577c12fc75f0b7fa76830e42e
Gage Weaver <Gage_Weaver@ku.edu>
2024-12-05 18:08:59 -0600

    Create fullbackend.h
    Header file for the full backend initializing the Backend class

---
+++ Expression_Validation_V1/fullbackend.h
@@ -0,0 +1,14 @@
+#ifndef fullBackend_H
+#define fullBackend_H
+
+#include <string>
+#include <vector>
+#include "tokenizeExpression.h"
+#include "allIncludes.h"
+
+class Backend {
+public:
+    int backendEvaluate(const std::string &input, std::vector<std::string> &output);
+};
+
+#endif
