1d36d69aa090aeedef84a6416b417c52a4f37103
Gage Weaver <Gage_Weaver@ku.edu>
2024-12-08 18:09:17 -0600

    Update Operator_Layer.cpp

--- Operator_Layer.cpp
+++ Operator_Layer.cpp
@@ -1,103 +1,30 @@
-/**
-Operator Class contains the following methods:
-    ● Addition(float a, float b): Adds two numbers.⁤ (a+b) and returns the answer
-    ● Subtraction(float a, float b): Subtracts the second number from the first. (a-b) and returns the
-        answer
-    ● Multiplication(float a, float b): Multiplies two numbers.⁤ (a*b) and returns the answer
-    ● Modulus(float a, float b): Returns the remainder of dividing the first number by the second number
-        and returns the answer
-    ● Divide(float a, float b): Returns the result of division, first number divided by second (a/b) and
-        returns the answer with safeguards for division by 0
-    ● Power(float a, float b): Computes the first integer to the power of the second and returns the
-        answer
- */
-
-
-#include <iostream>
-#include <cmath>
-#include <vector>
-#include "Operator_layer.h"
+#include "operator_layer.h"
 using namespace std;
- 
 
+double Operator::addition(double a, double b) {
+    return a + b;
+}
 
- class Operator{
-    public:
-    
-    char operation(float a,float b, char op){
-        switch (op) {
-        //  Addition
-        case '+':
-            cout << a << " + " << b << " = " << addition(a, b) << endl;
-            break;
-        //   Subtraction
-        case '-':
-            cout << a << " - " << b << " = " << subtraction(a, b) << endl;
-            break;
-        //    Multiplication
-        case '*':
-            cout << a << " * " << b << " = " << multiplcation(a, b) << endl;
-            break;
-        //     Division
-        case '/':
-            cout << a << " / " << b << " = " << division(a, b) << endl;
-            break;
-        //       Power
-        case '^':
-            cout << a << " ^ " << b << " = " << power_raise(a, b) << endl;
-            break;
-        //       Invalid input handling 
-        default:
-            cout << "Invalid choice" << endl;
-            cout << " TRY AGAIN"  << endl;
-            break;
-        }
-    }
- 
-    double Operator::addition(float a, float b) {
-        return a + b;
-    }
-    
-    double Operator::subtraction(float a, float b) {
-        return a - b;
-    }
-    
-    double Operator::(float a, float b) { 
-        if (b == 0) {
-            cout << "Error: Division by zero is not allowed." << endl;
-            return 0; 
-        }
-        return a/b; 
-    }
-    
-    double Operator::multiplcation(float a, float b) {
-        return a * b;
-    }
-    
-    double Operator::power_raise(float a, float b) {
-        return pow(a,b);
-    }
-};
+double Operator::subtraction(double a, double b) {
+    return a - b;
+}
 
-
-
-
-int main() {
-    float num_1;
-    float num_2;
-    char choice;
-    Operator Caculator; 
-    while (true){
-        cout << "Enter a number: ";
-        cin >> num_1;
-        cout << endl;
-        cout << "Enter operation to be performed (+, -, *, /, ^): ";
-        cin >> choice; // Read the choice from the user
-        cout << endl;
-        cout << "Enter another number: ";
-        cin >> num_2;
-        cout << endl;
-        Caculator.operation(num_1, num_2, choice);
+double Operator::division(double a, double b) {
+    if (b == 0) {
+        cout << "Error: Division by zero is not allowed." << endl;
+        return 0;
     }
-    return 0 ; 
+    return a / b;
+}
+
+double Operator::multiplication(double a, double b) {
+    return a * b;
+}
+
+double Operator::power_raise(double a, double b) {
+    return pow(a, b);
+}
+
+double Operator::modulus_operator(double a, double b){
+    return fmod(a, b);
 }
