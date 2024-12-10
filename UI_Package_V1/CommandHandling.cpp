385fe700b8c617a61c503912ca50e675e7eec9c0
DelaneyGray <159090659+DelaneyGray@users.noreply.github.com>
2024-12-09 20:06:58 -0600

    Update CommandHandling.cpp
    Added invalid & valid expression examples.

--- UI_Package_V1/CommandHandling.cpp
+++ UI_Package_V1/CommandHandling.cpp
@@ -20,13 +20,26 @@ void CommandHandling::help(){
     cout <<"(**) Exponentation"<< std::endl;
     cout <<"=========================================================================================="<<std::endl;
     cout <<"Common Errors to Avoid: "<< std::endl;
-    cout <<  "-Division by zero" << std::endl;
-    cout <<  "-Missing parenthesis" << std::endl;
-    cout <<  "-Invalid symbols" << std::endl;
+    cout <<"-Division by Zero"<< std::endl;
+    cout <<"-Missing Parenthesis"<< std::endl;
+    cout <<"-Invalid Symbols"<< std::endl;
+    cout <<"-Missing Operand/Operator"<< std::endl;
     cout <<"=========================================================================================="<<std::endl;
-    cout << "Valid expression examples: "<< std::endl;
-    //examples here
-    cout <<"Invalid expression examples: "<< std::endl;
-    //examples here
+    cout << "Valid Expression Examples: "<< std::endl;
+    cout <<"2 + 4"<< std::endl;
+    cout <<"6 - (2 + 3) "<< std::endl;
+    cout <<"15 * 2 / 3"<< std::endl;
+    cout <<"2 ** 3" << std::endl;
+    cout <<"4 * 3 - (3 + 2) % 2 - 1"<< std::endl;
+    cout <<"(((2 + 4)))"<< std::endl;
+    cout <<"=========================================================================================="<<std::endl;
+    cout <<"Invalid Expression Examples: "<< std::endl;
+    cout <<"4 / 0        < Division by Zero"<< std::endl;
+    cout <<"(3 - 2       < Missing Parenthesis"<< std::endl;
+    cout <<"13 & 2       < Invalid Symbol"<< std::endl;
+    cout <<"4 x 2        < Invalis Sumbol"<< std::endl;
+    cout <<"* 3 - 8      < Missing Operand"<< std::endl;
+    cout <<"2 * ( 1 - )  < Missing Operand"<< std::endl;
+    cout <<"3 (2 + 1)    < Missing Operator"<< std::endl;
     cout <<"=========================================================================================="<<std::endl;
 }
