c509fedfade2aa85fd00739d68c1917d91f71999
Gage Weaver <Gage_Weaver@ku.edu>
2024-12-10 20:50:05 -0600

    Fix Typos

--- FullProgram/commandhandling.cpp
+++ FullProgram/commandhandling.cpp
@@ -11,7 +11,7 @@ bool CommandHandling::exit(){
 void CommandHandling::help(){
     cout <<"HELP "<< std::endl;
     cout <<"=========================================================================================="<<std::endl;
-    cout <<"The expression parser accepts numeric constants, parenthesis, and the following operators"<<std::endl;
+    cout <<"The expression evaluator accepts numeric constants, parentheses, and the following operators"<<std::endl;
     cout <<"Valid operators: "<< std::endl;
     cout <<"(+)  Addition"<< std::endl;
     cout <<"(-)  Subtraction"<< std::endl;
@@ -22,7 +22,7 @@ void CommandHandling::help(){
     cout <<"=========================================================================================="<<std::endl;
     cout <<"Common Errors to Avoid: "<< std::endl;
     cout <<"-Division by Zero"<< std::endl;
-    cout <<"-Missing Parenthesis"<< std::endl;
+    cout <<"-Missing Parentheses"<< std::endl;
     cout <<"-Invalid Symbols"<< std::endl;
     cout <<"-Missing Operand/Operator"<< std::endl;
     cout <<"=========================================================================================="<<std::endl;
@@ -36,9 +36,9 @@ void CommandHandling::help(){
     cout <<"=========================================================================================="<<std::endl;
     cout <<"Invalid Expression Examples: "<< std::endl;
     cout <<"4 / 0        < Division by Zero"<< std::endl;
-    cout <<"(3 - 2       < Missing Parenthesis"<< std::endl;
+    cout <<"(3 - 2       < Missing Parentheses"<< std::endl;
     cout <<"13 & 2       < Invalid Symbol"<< std::endl;
-    cout <<"4 x 2        < Invalis Sumbol"<< std::endl;
+    cout <<"4 x 2        < Invalid Symbol"<< std::endl;
     cout <<"* 3 - 8      < Missing Operand"<< std::endl;
     cout <<"2 * ( 1 - )  < Missing Operand"<< std::endl;
     cout <<"3 (2 + 1)    < Missing Operator"<< std::endl;
